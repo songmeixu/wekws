7b3875547f59a3bef8207435d46194ab4384d1c4
meixu <songmeixu@outlook.com>
2023-04-07 11:41:25 +0800

    change portaudio URL
    As previous one doesn't work, for me at least, with error: "no device", when run stream_kws_main.

--- runtime/core/cmake/portaudio.cmake
+++ runtime/core/cmake/portaudio.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(portaudio
-  URL      https://github.com/PortAudio/portaudio/archive/refs/tags/v19.7.0.tar.gz
-  URL_HASH SHA256=5af29ba58bbdbb7bbcefaaecc77ec8fc413f0db6f4c4e286c40c3e1b83174fa0
+  URL      http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz
+  URL_HASH SHA256=47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def
 )
 FetchContent_MakeAvailable(portaudio)
 include_directories(${portaudio_SOURCE_DIR}/include)
